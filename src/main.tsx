6e6a164ee7d0fb3252490dc997df932933a93fd9
liangxiaojun <fatalxiao@163.com>
2023-09-28 15:41:41 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+/**
+ * @file main.tsx
+ */
+
+// Components
+import {StrictMode} from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App.tsx';
+
+// Vendors
+import ReactDOM from 'react-dom/client';
 
 // Style
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+    <StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </StrictMode>
 );
