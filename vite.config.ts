8dfdde393bf4e03c428ce85e6a380123407cfa3a
liangxiaojun <fatalxiao@163.com>
2023-09-28 15:03:47 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-})
+export default defineConfig(({/*command, */mode}) => {
+
+    // // dev specific config
+    // if (command === 'serve') {
+    //     return {};
+    // }
+    // // command === 'build'
+    // else {
+    //     return {};
+    // }
+
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        // vite config
+        define: {
+            __APP_ENV__: JSON.stringify(env.APP_ENV)
+        },
+        build: {
+            outDir: 'docs'
+        },
+        plugins: [
+            react()
+        ]
+    };
+
+});
